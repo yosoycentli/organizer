8da9047bf243bff899cd3f532df622d01ea4421b
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2024-04-26 13:18:15 -0600

    [ADD] participants to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tasks
+  has_many :participations, class_name: 'Participant'
+  has_many :tasks
 end
